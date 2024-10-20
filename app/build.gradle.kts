fd4b012f094204682b9d79719994ddc11a908e2d
D13G0TR <d.troncoso24@alumnos.santotomas.cl>
2024-10-20 19:34:55 -0300

    Integración FireBase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
+
 }
+
+
+
+
 
 android {
     namespace = "com.example.notisalud"
@@ -51,6 +57,7 @@ android {
 
 dependencies {
 
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -68,4 +75,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
